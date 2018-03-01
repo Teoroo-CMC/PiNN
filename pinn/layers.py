d939c40f237841f541d6c4afea9ec6eff47c50d2
yunqi <yunqi_shao@yahoo.com>
2018-03-01 11:20:36 +0800

    Merge two types of pi layers

--- pinn/layers.py
+++ pinn/layers.py
@@ -87,7 +87,44 @@ class ip_layer(pinn_layer_base):
 
 
 class pi_layer(pinn_layer_base):
-    '''Interaction pooling layer
+    '''Pairwise interaction layer
+    '''
+
+    def __init__(self,
+                 name,
+                 n_nodes=8,
+                 trainable=True,
+                 activation='tanh',
+                 variables=None):
+        pinn_layer_base.__init__(self, name, n_nodes, variables, trainable,
+                                 activation)
+        if variables is None:
+            variables = [{'name': '%s-w1' % self.name, 'val': None},
+                         {'name': '%s-w2' % self.name, 'val': None},
+                         {'name': '%s-w3' % self.name, 'val': None},
+                         {'name': '%s-b' % self.name, 'val': None}]
+        self.variables = variables
+
+    def process(self, i_nodes, p_nodes, i_mask, p_mask, i_kernel, dtype):
+
+        shapes = [[p_nodes[-1].shape[-1], self.n_nodes],
+                  [p_nodes[-1].shape[-1], self.n_nodes],
+                  [i_kernel.shape[-1], self.n_nodes],
+                  [1, 1, 1, self.n_nodes]]
+        w1, w2, w3, b = get_variables(self.variables, dtype, shapes)
+
+        act = tf.nn.__getattribute__(self.activation)
+        output = act(
+            tf.expand_dims(tf.tensordot(p_nodes[-1], w1, [[2], [0]]), 1) +
+            tf.expand_dims(tf.tensordot(p_nodes[-1], w2, [[2], [0]]), 2) + b)
+        output = output * tf.tensordot(i_kernel, w3, [[3],[0]])
+        output = tf.where(tf.tile(i_mask, [1, 1, 1, self.n_nodes]),
+                          output, tf.zeros_like(output))
+        i_nodes[-1] = tf.concat([i_nodes[-1], output], axis=-1)
+
+
+class kernel_pi(pinn_layer_base):
+    '''Pairwise interaction layer that acts on the kernel
     '''
 
     def __init__(self,
@@ -105,9 +142,9 @@ class pi_layer(pinn_layer_base):
         self.variables = variables
 
     def process(self, i_nodes, p_nodes, i_mask, p_mask, i_kernel, dtype):
-
-        shapes = [[p_nodes[-1].shape[-1], i_kernel.shape[-1], self.n_nodes],
-                  [p_nodes[-1].shape[-1], i_kernel.shape[-1], self.n_nodes],
+        i_kernel = tf.expand_dims(i_kernel, axis=4)
+        shapes = [[p_nodes[-1].shape[-1], i_kernel.shape[-2], self.n_nodes],
+                  [p_nodes[-1].shape[-1], i_kernel.shape[-2], self.n_nodes],
                   [1, 1, 1, self.n_nodes]]
         w1, w2,  b = get_variables(self.variables, dtype, shapes)
 
@@ -235,6 +272,29 @@ def default_layers(i_nodes=24, p_nodes=36, depth=1, act='tanh'):
         layers += [
             ii_layer('ii%i-0' % i, n_nodes=i_nodes//2, activation=act),
             pi_layer('pi%i-0' % i, n_nodes=i_nodes//2, activation=act),
+            pp_layer('pp%i-0' % i, n_nodes=p_nodes//3, activation=act),
+            ip_layer('ip%i-0' % i, n_nodes=p_nodes//3, activation=act,
+                     pool_type='sum'),
+            ip_layer('ip%i-1' % i, n_nodes=p_nodes//3, activation=act,
+                     pool_type='max'),
+            en_layer('en%i-0' % i, n_nodes=[p_nodes])
+        ]
+    return layers
+
+
+def default_layers_k(i_nodes=24, p_nodes=36, depth=1, act='tanh'):
+    layers = [
+        kernel_pi('pi1-0', n_nodes=i_nodes, activation=act),
+        ip_layer('ip1-0', n_nodes=p_nodes//3, activation=act,
+                 pool_type='sum'),
+        ip_layer('ip1-1', n_nodes=p_nodes//3, activation=act,
+                 pool_type='max'),
+        en_layer('en1-0', n_nodes=[p_nodes])
+    ]
+    for i in range(2, depth+1):
+        layers += [
+            ii_layer('ii%i-0' % i, n_nodes=i_nodes//2, activation=act),
+            kernel_pi('pi%i-0' % i, n_nodes=i_nodes//2, activation=act),
             pp_layer('pp%i-0' % i, n_nodes=p_nodes//3, activation=act),
             ip_layer('ip%i-0' % i, n_nodes=p_nodes//3, activation=act,
                      pool_type='sum'),
