4f7b6f645a931e35a480fde38c421e032b675ddb
yunqi <yunqi_shao@yahoo.com>
2018-05-22 16:34:07 +0800

    Add the quantum-simulation.org dataset parser

--- pinn/dataset.py
+++ pinn/dataset.py
@@ -3,13 +3,101 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Output shapes:
     'coord':   [natoms, 3]
-    'element': [natoms]
+    'atoms':   [natoms]
     'energy':  []
 
 """
 
 import os
 import tensorflow as tf
+
+
+class QSO_XML_dataset():
+    """XML format for first-principle simulations recommanded here
+    http://www.quantum-simulation.org/
+
+    The dataset on a image basis, meaning each xml file is splited to
+    three portions
+    """
+
+    def __init__(self, files, n_atoms=None,
+                 split_ratio=[0.8, 0.05, 0.05], shuffle=True, seed=None):
+        """
+        Args:
+            files (list): A list of xml files or one xml file
+            natoms (:obj:`int`, optional): The maximum number of atoms in the
+                dataset
+        """
+        from lxml import etree
+        import random
+
+        random.seed(seed)
+
+        random.shuffle(files)
+        self._train_list = []
+        self._test_list = []
+        self._vali_list = []
+
+        for fname in files:
+            tree = etree.parse(fname)
+            size = int(tree.findall('iteration')[-1].values()[0])
+            split_list = [i+1 for i in range(size)]
+            random.shuffle(split_list)
+            n_train = int(len(split_list) * split_ratio[0])
+            n_test = int(len(split_list) * sum(split_ratio[:2]))
+
+            self._train_list.append((fname, split_list[: n_train]))
+            self._test_list.append((fname, split_list[n_train: n_test]))
+            self._vali_list.append((fname, split_list[n_test:]))
+        self.n_atoms = n_atoms
+
+    def get_train(self, dtype=tf.float32):
+        dataset = _qso_xml_to_dataset(self._train_list, dtype)
+        return dataset
+
+    def get_test(self, dtype=tf.float32):
+        dataset = _qso_xml_to_dataset(self._vali_list, dtype)
+        return dataset
+
+    def get_vali(self, dtype=tf.float32):
+        dataset = _qso_xml_to_dataset(self._vali_list, dtype)
+        return dataset
+
+
+def _qso_xml_generator(filelist):
+    from lxml import etree
+    for fname, iters in filelist:
+        species = {}
+        for event, elem in etree.iterparse(fname):
+            if elem.tag == "species":
+                name = elem.values()[0]
+                atomic_number = elem.find('atomic_number')
+                species[name] = int(atomic_number.text)
+            if elem.tag == "iteration" and int(elem.values()[0]) in iters:
+                atomset = elem.find('atomset')
+                cell = atomset.find('unit_cell')
+                cell = [[float(i) for i in v.split()] for v in cell.values()]
+                atoms = atomset.findall('atom')
+                coord = [[float(i) for i in
+                          atom.find('position').text.split()]
+                         for atom in atoms]
+                atoms = [species[atom.values()[1]] for atom in atoms]
+                energy = float(elem.find('etotal').text)
+                data = {
+                    'coord': coord,
+                    'atoms': atoms,
+                    'e_data': energy
+                }
+                yield data
+
+
+def _qso_xml_to_dataset(filelist, dtype):
+    dataset = tf.data.Dataset.from_generator(
+        lambda: _qso_xml_generator(filelist),
+        {'coord': dtype, 'atoms': tf.int32, 'e_data': dtype},
+        {'coord': [None, 3], 'atoms': [None], 'e_data': []},
+    )
+    return dataset
 
 
 class ANI_H5_dataset():
@@ -55,14 +143,12 @@ class ANI_H5_dataset():
         n_test = int(len(data_list) * sum(split_ratio[:2]))
 
         self._train_list = data_list[:n_train]
-        self._test_list = data_list[:n_test]
+        self._test_list = data_list[n_train:n_test]
         self._vali_list = data_list[n_test:]
         self.n_atoms = n_atoms
-        self.shuffle = 1000000
 
     def get_train(self, dtype=tf.float32):
         dataset = _ani_to_dataset(self._train_list, dtype)
-        dataset.shuffle(self.shuffle)
         return dataset
 
     def get_test(self, dtype=tf.float32):
