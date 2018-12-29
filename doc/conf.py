a1a48cd9cf240b4decbad633ce8b2674bb014e11
Yunqi Shao <yunqi_shao@yahoo.com>
2018-12-29 19:57:45 +0100

    Add a logo for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -81,12 +81,13 @@ pygments_style = None
 #
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'images/logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'logo_only':True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
