e66f94eeea96154ef99c26d983eb25f91c9d1ebe
Yunqi Shao <yunqi@kemi.uu.se>
2019-09-03 12:10:15 +0200

    Build documentations for side branches

--- .circleci/build_doc.sh
+++ .circleci/build_doc.sh
@@ -4,29 +4,31 @@
 # taken from here: https://www.alkaline-ml.com/2018-12-23-automate-gh-builds/
 
 set -e
+shopt -s extglob
 
 cd doc
 make clean html
 cd ..
 
 mv doc/_build/html ./
-git stash
+rm -r !(".git"|"html"|".."|".")
+
+git checkout gh-pages
 git checkout --orphan gh-pages-tmp
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
-shopt -s extglob
-rm -r !(".git"|"html"|".."|".")
 touch .nojekyll
-mv html/* ./
-rm -r html/
 
 if [[ "$CIRCLE_BRANCH" =~ ^dev$|^[0-9]+\.[0-9]+\.X$ ]]; then
-    git add --all
-    git commit --allow-empty  -m "Publishing updated documentation..."
-    git remote rm origin
-    git remote add origin https://"$GH_NAME":"$GH_TOKEN"@github.com/teoroo-cmc/PiNN_dev.git
-    git push --force origin gh-pages-tmp:gh-pages
+    cp -r html/* ./
 else
-    echo "Not on dev, so won't push doc"
+    mkdir -p "$CIRCLE_BRANCH"
+    cp -r html/* ./"$CIRCLE_BRANCH"
 fi
 
+rm -r html/    
+git add --all
+git commit --allow-empty  -m "[ci skip] Publishing updated documentation..."
+git remote rm origin
+git remote add origin https://"$GH_NAME":"$GH_TOKEN"@github.com/teoroo-cmc/PiNN_dev.git
+git push --force origin gh-pages-tmp:gh-pages
