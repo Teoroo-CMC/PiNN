5c2d8d2639e39e8d57720bf33a3e44cc66c9f5aa
Yunqi Shao <yunqi@kemi.uu.se>
2019-12-17 13:16:44 +0100

    Workaround RTD building issue by using cpu version of TensorFlow
    Using the hack from readthedocs/readthedocs.org#5512

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
 from setuptools import setup, find_packages
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    INSTALL_REQUIRES = ['ase', 'numpy', 'pyyaml', 'tensorflow-cpu==1.15']
+else:
+    INSTALL_REQUIRES = ['ase', 'numpy', 'pyyaml', 'tensorflow==1.15']
 
 setup(name='pinn',
       version='dev',
@@ -8,7 +15,7 @@ setup(name='pinn',
       author_email='yunqi_shao@yahoo.com',
       license='BSD',
       packages=find_packages(),
-      install_requires=['ase', 'numpy', 'pyyaml', 'tensorflow==1.15'],
+      install_requires=INSTALL_REQUIRES,
       entry_points={
           'console_scripts': ['pinn_train=pinn.trainer:main']
       }
