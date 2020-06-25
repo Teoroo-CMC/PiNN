19675aefefc79136b3b4d855e723031c6f533039
Yunqi Shao <yunqi.shao@kemi.uu.se>
2020-06-25 14:24:00 +0200

    Set the version number in setup.py and __version__

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-import os
+import os, re
 from setuptools import setup, find_packages
 
+with open('pinn/__init__.py') as f:
+    version = re.search("__version__ = '(.*)'", f.read()).group(1)
+
 setup(name='pinn',
-      version='dev',
+      version=version,
       description='Pair interaction neural network',
       url='https://github.com/yqshao/pinn',
       author='Yunqi Shao',
