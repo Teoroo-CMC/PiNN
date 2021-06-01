135f9bb9087e20145d5b9500cea58b82354e96b8
Yunqi Shao <yunqi.shao@kemi.uu.se>
2021-06-01 13:35:05 +0200

    [network] name element-specific dense layers
    This fixes BPNN with disordered elements in NN_spec.

--- pinn/networks/bpnn.py
+++ pinn/networks/bpnn.py
@@ -381,7 +381,7 @@ class BPFeedForward(tf.keras.layers.Layer):
         self.out_units = out_units
         for k, v in nn_spec.items():
             self.ff_layers[k] = [
-                tf.keras.layers.Dense(units, activation=act)
+                tf.keras.layers.Dense(units, activation=act, name=f'dense_{k}')
                 for units in v]
             self.ff_layers[k].append(
                 tf.keras.layers.Dense(out_units, activation=None,
