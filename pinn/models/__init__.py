b117675b380d156eec52c125768ba7223b33ca4f
Yunqi Shao <yunqi.shao@kemi.uu.se>
2021-06-01 12:11:07 +0200

    [core] better error message for get_model

--- pinn/models/__init__.py
+++ pinn/models/__init__.py
@@ -15,12 +15,17 @@ def get(model_spec, **kwargs):
     if isinstance(model_spec, str):
         if tf.io.gfile.exists('{}/params.yml'.format(model_spec)):
             params_file = os.path.join(model_spec, 'params.yml')
+            with FileIO(params_file, 'r') as f:
+                model_spec = dict(yaml.load(f, Loader=yaml.Loader),
+                                  model_dir=model_spec)
         elif tf.io.gfile.exists(model_spec):
             params_file = model_spec
-        with FileIO(params_file, 'r') as f:
-            model_spec = yaml.load(f, Loader=yaml.Loader)
-    # Write the model parameter
+            with FileIO(params_file, 'r') as f:
+                model_spec = yaml.load(f, Loader=yaml.Loader)
+        else:
+            raise ValueError(f'{model_spec} does not seem to be a parameter file or model_dir')
     else:
+        # we have a dictionary, write the model parameter
         model_dir = model_spec['model_dir']
         yaml.Dumper.ignore_aliases = lambda *args: True
         to_write = yaml.dump(model_spec)
