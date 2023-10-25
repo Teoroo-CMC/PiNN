[bug] Replace redundant bond vector calculation

pinn/models/atomic_dipole.py
@@ -48,21 +48,7 @@ def dipole_model(tensors, labels, mode, params):
     atom_rind, pair_rind = make_indices(tensors)
 
     # Compute bond vector
-    R = tf.fill([nbatch, nmax, 3], np.nan)
-    R = tf.tensor_scatter_nd_update(R, atom_rind, coord)
-    R_ij = R[:,None,:,:] - R[:,:,None,:]
-    
-    # Apply minimum image convention if there exists a cell
-    cell = tensors['cell'] if 'cell' in tensors is not None
-    if cell is not None:
-        R_ij_flat = tf.reshape(tf.transpose(R_ij, [0,3,1,2]), [nbatch, 3, -1])
-        cell_inv = tf.linalg.inv(cell)
-        R_ij_frac = tf.einsum('bxc,bxp->bcp', cell_inv, R_ij_flat)
-        R_ij_frac -= tf.math.rint(R_ij_frac)
-        R_ij_flat = tf.einsum('bcx,bcp->bxp', cell, R_ij_frac)
-        R_ij = tf.transpose(tf.reshape(R_ij_flat,[nbatch,3,nmax,nmax]),[0,2,3,1])
-
-    R_ij = tf.where(tf.math.is_nan(R_ij), tf.zeros_like(R_ij), R_ij)
+    disp_r = tensors['diff']
 
     # Compute the total charge per structure in the batch
     charge = tf.math.unsorted_segment_sum(ppred, ind1[:, 0], nbatch)