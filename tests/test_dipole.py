fe3a37b15dace7f22ebaa9465b8cc965a662a715
LKnijff <lisanne.knijff@gmail.com>
2023-11-06 20:12:56 +0100

    [test] Add part of dipole test

--- tests/test_dipole.py
+++ tests/test_dipole.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-
+import tempfile
+import pytest
+import tensorflow as tf
+from pinn.io import load_numpy, sparse_batch
+from shutil import rmtree
 def _get_dipole_data():
     from ase.build import molecule
 
@@ -27,3 +31,51 @@ def _get_dipole_data():
             }
 
     return data
+
+
+@pytest.mark.forked
+def test_pinn_atomic_dipole():
+    testpath = tempfile.mkdtemp()
+    network_params = {
+        'ii_nodes': [8, 8],
+        'pi_nodes': [8, 8],
+        'pp_nodes': [8, 8],
+        'out_nodes': [8, 8],
+        'depth': 3,
+        'rc': 5.,
+        'n_basis': 5,
+        'atom_types': [1]
+    }
+    params = {
+        'model_dir': testpath,
+        'network': {
+            'name': 'PiNet',
+            'params': network_params},
+        'model': {
+            'name': 'atomic_dipole_model'}}
+    _atomic_dipole_tests(params)
+    rmtree(testpath)
+
+
+
+def _atomic_dipole_tests(params):
+    # Series of tasks that the atomic dipole model should pass
+    import pinn
+    from pinn.calculator import PiNN_calc
+
+    data = _get_dipole_data()
+
+    def train(): return load_numpy(data).repeat().shuffle(
+        500).apply(sparse_batch(50))
+
+    def test(): return load_numpy(data).apply(sparse_batch(10))
+    train_spec = tf.estimator.TrainSpec(input_fn=train, max_steps=1e3)
+    eval_spec = tf.estimator.EvalSpec(input_fn=test, steps=100)
+
+    model = pinn.get_model(params)
+    results, _ = tf.estimator.train_and_evaluate(model, train_spec, eval_spec)
+    
+    assert np.allclose(results['METRICS/E_RMSE']/params['model']['params']['e_scale'],
+                       np.sqrt(np.mean((e_pred/params['model']['params']['e_unit']
+                                        - data['e_data'])**2)), rtol=1e-2)
+
