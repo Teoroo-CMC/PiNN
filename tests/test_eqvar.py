bcf4b239ab33dec2e7904138d395c9783ed42a26
Roy Kid <lijichen365@gmail.com>
2024-06-18 15:00:59 +0200

    [ut] add ut for modularized pinet2

--- tests/test_eqvar.py
+++ tests/test_eqvar.py
@@ -1,14 +1,9 @@
 import tensorflow as tf
 import numpy as np
-import random
+import pytest
 from pinn.networks.pinet import PiNet
 from pinn.networks.pinet2 import PiNet2
-from pinn.networks.pinet2_p5_dot import PiNet2P5Dot
-from pinn.networks.pinet2_p5_prod import PiNet2P5Prod
-from pinn.networks.pinet2_p5_dot import PiNet2P5Dot
-from pinn.networks.pinet2_p5_prod import PiNet2P5Prod
-from pinn.networks.pinet2_p5_combine import PiNet2P5Combine
-from utils import rotate, create_rot_mat
+from utils import rotate
 
 
 class TestEquivar:
@@ -41,8 +36,12 @@ class TestEquivar:
             energy2 = pinet(batch2)
             tf.debugging.assert_near(energy1, energy2)
 
-    def test_pinet2_p5_dot(self, mocked_data):
-        pinet = PiNet2P5Dot(
+    @pytest.mark.parametrize('rank', [1, 3, 5])
+    def test_pinet2_refactor(self, mocked_data, rank):
+        from pinn.networks.pinet2_modularized import PiNet2
+
+        pinet = PiNet2(
+            rank=rank,
             atom_types=[0, 1],
         )
 
@@ -53,155 +52,25 @@ class TestEquivar:
             batch2 = batch.copy()
             batch2['coord'] = rotate(batch2['coord'], 42.)
             energy2 = pinet(batch2)
-            np.testing.assert_allclose(energy1, energy2, rtol=1e-4, atol=1e-4)
+            tf.debugging.assert_near(energy1, energy2)
 
-    def test_pinet2_p5_prod(self, mocked_data):
-        pinet = PiNet2P5Prod(
+    def test_pinet2_refactor(self, mocked_data):
+        from pinn.networks.pinet2_modularized import PiNet2
+
+        pinet = PiNet2(
+            rank=3,
             atom_types=[0, 1],
+            out_extra={
+                f'p3': 16,
+            }
         )
 
         for batch in mocked_data:
             batch1 = batch.copy()
-            energy1 = pinet(batch1)
+            energy1, actual_extra = pinet(batch1)
 
             batch2 = batch.copy()
             batch2['coord'] = rotate(batch2['coord'], 42.)
-            energy2 = pinet(batch2)
-            np.testing.assert_allclose(energy1, energy2, rtol=1e-3, atol=1e-3)
-
-    def test_pinet2_p5_irrep_dot(self, mocked_data):
-        pinet = PiNet2P5Dot(
-            atom_types=[0, 1],
-        )
-        
-        for batch in mocked_data:
-            batch1 = batch.copy()
-            energy1 = pinet(batch1)
-
-            batch2 = batch.copy()
-            batch2['coord'] = rotate(tf.cast(batch2['coord'], tf.float32), random.randint(0, 4123))
-            energy2 = pinet(batch2)
-            np.testing.assert_allclose(energy1, energy2, rtol=1e-4, atol=1e-4)
-
-    def test_pinet2_p5_irrep_prod(self, mocked_data):
-        pinet = PiNet2P5Prod(
-            atom_types=[0, 1],
-        )
-        
-        for batch in mocked_data:
-            batch1 = batch.copy()
-            energy1 = pinet(batch1)
-
-            batch2 = batch.copy()
-            batch2['coord'] = rotate(tf.cast(batch2['coord'], tf.float32), random.randint(0, 4123))
-            energy2 = pinet(batch2)
-            np.testing.assert_allclose(energy1, energy2, rtol=1e-4, atol=1e-4)
-
-    def test_pinet2_p5_irrep_combine(self, mocked_data):
-        pinet = PiNet2P5Combine(
-            atom_types=[0, 1],
-        )
-        
-        for batch in mocked_data:
-            batch1 = batch.copy()
-            energy1 = pinet(batch1)
-
-            batch2 = batch.copy()
-            batch2['coord'] = rotate(tf.cast(batch2['coord'], tf.float32), random.randint(0, 4123))
-            energy2 = pinet(batch2)
-            np.testing.assert_allclose(energy1, energy2, rtol=1e-4, atol=1e-4)
-
-
-    def test_pinet2_p5_irrep_prod_internal(self, mocked_data):
-        from pinn.layers import (
-            CellListNL,
-            CutoffFunc,
-            PolynomialBasis,
-            GaussianBasis,
-            AtomicOnehot,
-            ANNOutput,
-        )
-        from pinn.networks.pinet import ResUpdate
-        from pinn.networks.pinet2_p5_dot import OutLayer
-        from pinn.networks.pinet2_p5_prod import GCBlock, PreprocessLayer
-
-        atom_types = [0, 1]
-        depth = 5
-        rc = 5.0
-        cutoff_type = 'f1'
-        basis_type = "polynomial"
-        n_basis = 4
-        gamma = 3.0
-        center = None
-
-        pp_nodes=[16, 16]
-        pi_nodes=[16, 16]
-        ii_nodes=[16, 16]
-        out_nodes=[16, 16]
-        out_units=1
-        out_pool=False
-        act="tanh"
-        weighted=True
-
-        preprocess = PreprocessLayer(atom_types, rc)
-        cutoff = CutoffFunc(rc, cutoff_type)
-
-        if basis_type == "polynomial":
-            basis_fn = PolynomialBasis(n_basis)
-        elif basis_type == "gaussian":
-            basis_fn = GaussianBasis(center, gamma, rc, n_basis)
-
-        res_update1 = [ResUpdate() for i in range(depth)]
-        res_update3 = [ResUpdate() for i in range(depth)]
-        res_update5 = [ResUpdate() for i in range(depth)]
-        gc_blocks = [
-            GCBlock(weighted, pp_nodes, pi_nodes, ii_nodes, activation=act)
-            for i in range(depth)
-        ]
-        out_layers = [OutLayer(out_nodes, out_units) for i in range(depth)]
-        # ann_output = ANNOutput(out_pool)
-
-        def call(tensors):
-            tensors = preprocess(tensors)
-            tensors["p3"] = tf.zeros([tf.shape(tensors["ind_1"])[0], 3, 1])
-            tensors["p5"] = tf.zeros([tf.shape(tensors["ind_1"])[0], 5, 1])
-            fc = cutoff(tensors["dist"])
-            basis = basis_fn(tensors["dist"], fc=fc)
-            output = 0.0
-            for i in range(depth):
-                p1, p3, p5 = gc_blocks[i](
-                    [tensors["ind_2"], tensors["p1"], tensors["p3"], tensors["p5"], tensors["norm_diff"], tensors["diff_p5"], basis]
-                )
-                output = out_layers[i]([tensors["ind_1"], p1, p3, output])
-                tensors["p1"] = res_update1[i]([tensors["p1"], p1])
-                tensors["p3"] = res_update3[i]([tensors["p3"], p3])
-                tensors["p5"] = res_update5[i]([tensors["p5"], p5])
-
-            return tensors
-
-        def rotate_batch(x, theta):
-            ndim = x.ndim - 2
-            rot = create_rot_mat(theta)
-            if ndim == 0:
-                return tf.einsum('xc, xy->yc', x, rot)
-            elif ndim == 1:
-                return tf.einsum('ixc, xy->iyc', x, rot)
-            elif ndim == 2:
-                return tf.einsum('xw, ixyc, yz->iwzc', rot, x, rot)
-
-        for batch in mocked_data:
-            rnd = random.randint(0, 42)
-    
-            batch1 = batch.copy()
-            tensors1 = call(batch1)
-
-            batch2 = batch.copy()
-            batch2['coord'] = rotate(tf.cast(batch2['coord'], tf.float32), rnd)
-            tensors2 = call(batch2)
-
-            np.testing.assert_allclose(tensors1['p1'], tensors2['p1'], rtol=1e-4, atol=1e-4)
-            np.testing.assert_allclose(
-                rotate_batch(tensors1['p3'], rnd), tensors2['p3'], rtol=1e-4, atol=1e-4
-            )
-
-
+            energy2, expect_extra = pinet(batch2)
+            tf.debugging.assert_near(energy1, energy2)
+            tf.debugging.assert_near(rotate(actual_extra['p3'], 42), expect_extra['p3'])
--- tests/test_layer.py
+++ tests/test_layer.py
@@ -4,14 +4,15 @@ import numpy as np
 import numpy.testing as npt
 import random
 from pinn.networks.pinet2 import DotLayer, ScaleLayer, PIXLayer
-from pinn.networks.pinet2_p5_prod import TensorProductLayer
-from utils import rotate, create_rot_mat
+from pinn.networks.pinet2_modularized import EquiVarLayer
+from utils import rotate
+
 
 class TestPiNet2:
 
     @pytest.mark.forked
     def test_pixlayer_rank2(self):
-            
+
         n_atoms = 10
         n_dims = 3
         n_channels = 5
@@ -23,9 +24,7 @@ class TestPiNet2:
         theta = 42
         actual = layer([ind_2, rotate(px, theta)])
         expect = rotate(layer([ind_2, px]), theta)
-        tf.debugging.assert_near(
-            actual, expect
-        )
+        tf.debugging.assert_near(actual, expect)
 
     @pytest.mark.forked
     def test_dotlayer_rank2(self):
@@ -37,11 +36,11 @@ class TestPiNet2:
         prop = tf.random.uniform((n_atoms, n_dims, n_dims, n_channels))
         prop_shape = prop.shape
         dot = DotLayer(weighted=True)
-        actual = dot(tf.reshape(rotate(prop, 42.), (prop_shape[0], -1, prop_shape[-1])))
+        actual = dot(
+            tf.reshape(rotate(prop, 42.0), (prop_shape[0], -1, prop_shape[-1]))
+        )
         expect = dot(tf.reshape(prop, (prop_shape[0], -1, prop_shape[-1])))
-        tf.debugging.assert_near(
-            actual, expect
-        )        
+        tf.debugging.assert_near(actual, expect)
 
     @pytest.mark.forked
     def test_scalelayer_rank2(self):
@@ -57,28 +56,9 @@ class TestPiNet2:
         out = scaler([px, p1[:, None, :]])
         assert out.shape == (n_atoms, n_dims, n_dims, n_channels)
         tf.debugging.assert_near(
-            rotate(scaler([px, p1[:, None, :]]), 42.), scaler([rotate(px, 42.), p1[:, None, :]])
+            rotate(scaler([px, p1[:, None, :]]), 42.0),
+            scaler([rotate(px, 42.0), p1[:, None, :]]),
         )
-
-
-    @pytest.mark.forked
-    def test_prodlayer_rank2(self):
-
-        n_atoms = 10
-        n_dims = 3
-        n_channels = 5
-
-        p3 = tf.random.uniform((n_atoms, n_dims, n_channels))
-        p5 = tf.random.uniform((n_atoms, n_dims, n_dims, n_channels))
-
-        layer = TensorProductLayer()
-        out = layer([p3, p5])
-        assert out.shape == (n_atoms, n_channels)
-        actual = layer([p3, p5])
-        expect = layer([rotate(p3, 42.), rotate(p5, 42.)])
-        np.testing.assert_allclose(
-            actual, expect, rtol=1e-5, atol=1e-5
-        )   
 
     @pytest.mark.forked
     def test_simple_dotlayer(self):
@@ -90,11 +70,9 @@ class TestPiNet2:
         prop = tf.random.uniform((n_atoms, n_dims, n_channels))
 
         dot = DotLayer(weighted=False)
-        actual = dot(rotate(prop, 42.))
+        actual = dot(rotate(prop, 42.0))
         expect = dot(prop)
-        tf.debugging.assert_near(
-            actual, expect
-        )
+        tf.debugging.assert_near(actual, expect)
 
     @pytest.mark.forked
     def test_general_dotlayer(self):
@@ -104,12 +82,10 @@ class TestPiNet2:
         n_channels = 5
 
         prop = tf.random.uniform((n_atoms, n_dims, n_channels))
-        theta = 42.
+        theta = 42.0
 
         dot = DotLayer(weighted=True)
-        tf.debugging.assert_near(
-            dot(prop), dot(rotate(prop, theta))
-        )
+        tf.debugging.assert_near(dot(prop), dot(rotate(prop, theta)))
 
     @pytest.mark.forked
     def test_scalelayer(self):
@@ -142,7 +118,7 @@ class TestPiNet2:
         out = pix([ind_2, px])
         assert out.shape == (n_pairs, n_dims, n_channels)
         tf.debugging.assert_near(
-            rotate(pix([ind_2, px]), 42.), pix([ind_2, rotate(px, 42.)])
+            rotate(pix([ind_2, px]), 42.0), pix([ind_2, rotate(px, 42.0)])
         )
 
     @pytest.mark.forked
@@ -159,7 +135,5 @@ class TestPiNet2:
         out = pix([ind_2, px])
         assert out.shape == (n_pairs, n_dims, n_channels)
         tf.debugging.assert_near(
-            rotate(pix([ind_2, px]), 42.), pix([ind_2, rotate(px, 42.)])
+            rotate(pix([ind_2, px]), 42.0), pix([ind_2, rotate(px, 42.0)])
         )
-
- 
